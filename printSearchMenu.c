f1162790967297bd4cec6ad57405905cb339f869
yuebanquan <yuebanquan@gmail.com>
2020-01-06 01:19:00 +0800

    修改查询菜单

--- printSearchMenu.c
+++ printSearchMenu.c
@@ -2,11 +2,13 @@
 
 //打印搜索菜单
 void printSearchMenu() {
-	//system("cls");		// 清屏
+	system("cls");		// 清屏
 	system("color 71");	//设置字体和背景颜色
 
 	putchar('\n');
 	printf("\t\t图书信息管理系统\t\n");
+	printf("\t ============================\n");
+	printf("\t\t  图书信息查询\t\n");
 	printf("\t ============================\n");
 	printf("\t ｜\t1.查询所有图书信息 ｜\n");
 	printf("\t ｜\t2.根据编号查询     ｜\n");
